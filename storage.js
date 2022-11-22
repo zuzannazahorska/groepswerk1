e0627638a8fc85f5cec77f147735dbf39c034af5
Zuzanna <zahorska.z96@gmail.com>
2022-11-22 15:59:55 +0100

    splitted the storage files

--- storage.js
+++ storage.js
@@ -33,17 +33,4 @@ submitbtn.addEventListener("click", function () {
   }
 });
 
-// let local = JSON.parse(localStorage.getItem('dataObject'));
-let session = JSON.parse(sessionStorage.getItem('dataObject'));
 
-
-      
-
-console.log('session:' + sessionStorage.getItem('dataObject'));
-// console.log('local: ' + localStorage.getItem('dataObject'));
-
-document.getElementById('name').innerHTML = session.inputname;
-
-let currentYear = new Date().getFullYear()  // returns the current year
-let age = currentYear - session.inputdate
-document.getElementById('age').innerHTML = age;
