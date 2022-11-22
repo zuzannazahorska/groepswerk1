fe166fe0ce196996bc852353a132af6ae97e6638
MARI_VILLALOBOS_GAT <marivillalobos93cr@gmail.com>
2022-11-22 16:39:46 +0100

    update

--- storage2.js
+++ storage2.js
@@ -1,14 +1,13 @@
 // let local = JSON.parse(localStorage.getItem('dataObject'));
-let session = JSON.parse(sessionStorage.getItem('dataObject'));
+let session = JSON.parse(sessionStorage.getItem("dataObject"));
 
-
-      
-
-console.log('session:' + sessionStorage.getItem('dataObject'));
+console.log("session:" + sessionStorage.getItem("dataObject"));
 // console.log('local: ' + localStorage.getItem('dataObject'));
 
-document.getElementById('name').innerHTML = session.inputname;
+document.getElementById("name").innerHTML = session.inputname;
 
-let currentYear = new Date().getFullYear()  // returns the current year
-let age = currentYear - session.inputdate
-document.getElementById('age').innerHTML = age;
+let currentYear = new Date().getFullYear(); // returns the current year
+let age = currentYear - session.inputdate;
+document.getElementById("age").innerHTML = age;
+
+console.log(mySession);
