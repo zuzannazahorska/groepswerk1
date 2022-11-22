ce5f8c9ac8c9e1fcd4ff5831f245cdc7277c1b89
Lenadubovets <lenadubovets@hotmail.com>
2022-11-22 11:09:52 +0100

    updeted

--- weather.js
+++ weather.js
@@ -1,15 +1,23 @@
 // fetch van jsonplaceholder API;
+
+let mySession = JSON.parse(sessionStorage.getItem("dataObject"));
+console.log(mySession);
+
 let weather = document.getElementById("weather");
-
 let url =
-  "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/brussels?unitGroup=metric&include=current&key=P9J7ZGZ9SBGSZQTDBMT86WNN3&contentType=json";
-
+  "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" +
+  mySession.inputcity +
+  "?unitGroup=metric&include=current&key=P9J7ZGZ9SBGSZQTDBMT86WNN3&contentType=json";
 fetch(url)
   .then((response) => response.json())
   .then((data) => {
     console.log(data);
     weather.innerHTML = `
-    <p>Location: ${data.resolvedAddress}</p>
-    <p>Temperature: ${data.currentConditions.temp}°C</p>
-    <p>Current conditions: ${data.currentConditions.conditions}</p>`;
+  <p>Location: ${data.resolvedAddress}</p>
+  <p>Temperature: ${data.currentConditions.temp}°C</p>
+  <p>Current conditions: ${data.currentConditions.conditions}</p>`;
+  })
+  .catch((err) => {
+    alert("Wrong city name");
+    console.log(err);
   });
